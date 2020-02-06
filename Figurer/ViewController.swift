082a7dca686bf112a221f0be2b857c48fab1856e
Zeqe G <zeqe.g@icloud.com>
2020-02-05 18:36:29 -0800

    first commit

---
+++ Figurer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Figurer
+//
+//  Created by Zeqiel Golomb on 2/5/20.
+//  Copyright © 2020 Zeqe Golomb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
