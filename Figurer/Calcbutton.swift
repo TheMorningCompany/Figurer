8cc30f09761cbd764cbc06a589b1cfbc45e39be1
Zeqe G <zeqe.g@icloud.com>
2021-01-10 15:50:30 -0800

    down with neumorphism

---
+++ Figurer/Calcbutton.swift
@@ -0,0 +1,17 @@
+//
+//  Calcbutton.swift
+//  Figurer
+//
+//  Created by Zeqiel Golomb on 1/10/21.
+//  Copyright © 2021 The Morning Company. All rights reserved.
+//
+
+import UIKit
+
+class Calcbutton: UIButton {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = frame.size.width/2
+    }
+
+}
